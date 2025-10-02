3c82b83f925739cf14d43adb140846c984aeefa1
nourhassan94 <70887474+nourhassan94@users.noreply.github.com>
2025-10-02 12:08:58 +0200

    test push
    test push

--- src/test/java/dev/library/LibraryTest.java
+++ src/test/java/dev/library/LibraryTest.java
@@ -8,14 +8,13 @@ public class LibraryTest {
 
     @Test
     public void testUserCanOnlyBorrowOneBookPerDay() {
-        // Arrange
         Library library = new Library();
         
-        // Act - Try to borrow two books in the same "day"
+        
         library.borrowBook("Harry Potter");
         library.borrowBook("Hitchhiker's guide to the galaxy");
         
-        // Assert - User should only have 1 book borrowed, not 2
+     
         assertEquals("User should only be able to borrow one book per day", 
                      1, library.listBorrowedBooks(false).size());
     }
