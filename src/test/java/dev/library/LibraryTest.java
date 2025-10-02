b1b6f823db8376effec3f77d0678b74e81c7297c
nourhassan94 <70887474+nourhassan94@users.noreply.github.com>
2025-10-02 13:15:11 +0200

    testnour
    test push

--- src/test/java/dev/library/LibraryTest.java
+++ src/test/java/dev/library/LibraryTest.java
@@ -1,5 +1,22 @@
+package dev.library;
 
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class LibraryTest {
-    
+
+    @Test
+    public void testUserCanOnlyBorrowOneBookPerDay() {
+        Library library = new Library();
+        
+        
+        library.borrowBook("Harry Potter");
+        library.borrowBook("Hitchhiker's guide to the galaxy");
+        
+     
+        assertEquals("User should only be able to borrow one book per day", 
+                     1, library.listBorrowedBooks(false).size());
+    }
+    //hejdågg
 }
