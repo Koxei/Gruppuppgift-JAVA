2409b51985bd45d1770506a6881ef1de762f6a62
Hamza-HF <hamza.hf44@gmail.com>
2025-10-06 13:06:22 +0200

    Hamza tester
    lånar 5 böcker och lånar en bok i 7 dagar

--- src/test/java/dev/library/LibraryTest.java
+++ src/test/java/dev/library/LibraryTest.java
@@ -1,21 +1,77 @@
 package dev.library;
 
-
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
+    private Library library;
 
+    @Before
+    public void setUp() {
+        library = new Library();
+    }
+     
     @Test
-    public void testUserCanOnlyBorrowOneBookPerDay() {
-        Library library = new Library();
-        
-        library.borrowBook("Simon");
-        library.borrowBook("Hello book");
-        
-        assertEquals("User should only be able to borrow one book per day", 
-                     1, library.listBorrowedBooks(false).size());
+    public void testBorrowUpToFiveBooks() {
+        library.borrowBook("Harry Potter");
+        library.borrowBook("Hitchhiker's guide to the galaxy");
+        library.borrowBook("It ends with us");
+        library.borrowBook("Ondskan");
+        library.borrowBook("Tempelriddaren");
+
+        ArrayList<Book> borrowed = library.listBorrowedBooks(false);
+        assertEquals(5, borrowed.size());
     }
     
-    // safsdfsdg
+    @Test
+    public void testCannotBorrowMoreThanFiveBooks() {
+        library.borrowBook("Harry Potter");
+        library.borrowBook("Hitchhiker's guide to the galaxy");
+        library.borrowBook("It ends with us");
+        library.borrowBook("Ondskan");
+        library.borrowBook("Tempelriddaren");
+        library.borrowBook("Harry Potter");
+
+        ArrayList<Book> borrowed = library.listBorrowedBooks(false);
+        assertEquals(5, borrowed.size());
+    }
+
+    @Test
+    public void testReturnBookDecreasesBorrowedCount() {
+        library.borrowBook("Harry Potter");
+        library.returnBook("Harry Potter");
+
+        ArrayList<Book> borrowed = library.listBorrowedBooks(false);
+        assertEquals(0, borrowed.size());
+    }
+    @Test
+    public void testCannotBorrowForMoreThanSevenDays() {
+    library.borrowBook("Harry Potter", 10); 
+
+    
+    assertEquals(0, library.listBorrowedBooks(false).size());
+}
+    @Test
+public void testCanBorrowForFiveDays() {
+    library.borrowBook("Harry Potter", 5); 
+
+    
+    assertEquals(0, library.listBorrowedBooks(false).size());
+}
+
+@Test
+public void testBorrowForMoreThanTwoDays() {
+    library.borrowBook("Harry Potter", 2); //
+
+   
+    assertEquals(0, library.listBorrowedBooks(false).size());
+}
+
+
+
+
 }
